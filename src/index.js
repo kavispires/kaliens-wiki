a11400f44f0b2a41e33f56c11e6f56259c52e690
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2018-03-04 13:38:44 -0800

    Base Files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+import store from './store';
+import routes from './routes';
+
+import './stylesheets/index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    { routes}
+  </Provider>, document.getElementById('root'));
+registerServiceWorker();
